51e7450863e7bc12324e78556ecc1fc5ea2562cd
niteshkamboj2811 <78463776+niteshkamboj2811@users.noreply.github.com>
2021-03-25 23:26:17 -0700

    Time: 398 ms (10.41%), Space: 0B (100.00%) - LeetHub

--- exchange-seats/exchange-seats.sql
+++ exchange-seats/exchange-seats.sql
@@ -6,10 +6,18 @@
 # END AS id, student
 # FROM seat
 
-SELECT CASE
-WHEN id % 2 = 0 THEN id - 1
-WHEN id = (SELECT COUNT(id) FROM seat) THEN id
-ELSE id + 1
-END AS id, student
+# SELECT CASE
+# WHEN id % 2 = 0 THEN id - 1
+# WHEN id = (SELECT COUNT(id) FROM seat) THEN id
+# ELSE id + 1
+# END AS id, student
+# FROM seat
+# ORDER BY 1
+
+SELECT id, IFNULL(CASE 
+WHEN id % 2 = 0 THEN LAG(student, 1) OVER (ORDER BY id)
+WHEN id % 2 = 1 THEN LEAD(student, 1) OVER (ORDER BY id)
+ELSE NULL
+END, student) AS student
 FROM seat
 ORDER BY 1
