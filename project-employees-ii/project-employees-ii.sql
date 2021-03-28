1b355b7a25ec4eff8947df50cc09058fbef22361
niteshkamboj2811 <78463776+niteshkamboj2811@users.noreply.github.com>
2021-03-28 00:17:17 -0700

    Time: 1039 ms (10.18%), Space: 0B (100.00%) - LeetHub

--- project-employees-ii/project-employees-ii.sql
+++ project-employees-ii/project-employees-ii.sql
@@ -1,13 +1,19 @@
 # Write your MySQL query statement below
-WITH test_cte AS (
-SELECT project_id, RANK() OVER (ORDER BY COUNT(employee_id) DESC) AS rrank
+# WITH test_cte AS (
+# SELECT project_id, RANK() OVER (ORDER BY COUNT(employee_id) DESC) AS rrank
+#     FROM project
+
+#     GROUP BY 1)
+
+# SELECT project_id
+# FROM test_cte
+# WHERE rrank = 1
+
+with test_cte AS (
+SELECT project_id, COUNT(DISTINCT employee_id) AS n
     FROM project
     GROUP BY 1)
 
 SELECT project_id
 FROM test_cte
-WHERE rrank = 1
-
-
-
-# RANK() OVER (ORDER BY COUNT(employee_id) DESC) AS num_ranking
+WHERE n = (SELECT MAX(n) FROM test_cte)
