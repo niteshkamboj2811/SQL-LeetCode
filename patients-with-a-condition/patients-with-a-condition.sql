821ef410e5f22481ffb59b6edf145219b4c5416c
niteshkamboj2811 <78463776+niteshkamboj2811@users.noreply.github.com>
2021-04-15 23:19:06 -0700

    Time: 280 ms (64.54%), Space: 0B (100.00%) - LeetHub

---
+++ patients-with-a-condition/patients-with-a-condition.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT *
+FROM patients
+WHERE conditions LIKE '% DIAB1%' or 
+conditions LIKE 'DIAB1%';
+
+# CONDITIONS LIKE '% DIAB1%' OR
+# CONDITIONS LIKE 'DIAB1%';
